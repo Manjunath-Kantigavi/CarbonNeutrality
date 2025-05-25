0425bd4b287dd80405be7699f646def9a295f76d
Manjunath-Kantigavi <mkantigavi@gmail.com>
2025-05-25 13:37:49 +0530

    Update vite.config.js and other latest changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/CarbonNeutrality/", // Ensure this matches your repository name
+  base: process.env.VITE_BASE_PATH || "/CarbonNeutrality", // Ensure this matches your repository name
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx"], // Ensure Vite processes JSX
